c42e77c9b04f4969906692f83f050cb1296dbd8d
Nicolas Möhlinger <nicocmoh@gmail.com>
2023-08-16 21:10:04 -0300

    primer reentrega

--- My-app-backend/index.js
+++ My-app-backend/index.js
@@ -8,16 +8,23 @@ class ProductManager {
         return this.products;
     }
 
-    addProducts(product) {
-        if (this.products.some(prod => prod.code === product.code)) {
+    addProducts(title, description, price, thumbnail, code, stock) {
+        const fields = [title, description, price, thumbnail, code, stock];
+        if (fields.some(field => field === undefined || field === null)) {
+            throw new Error("Todos los campos son obligatorios");
+        }
+
+        const codeExist = this.products.some(product => product.code === code);
+        if (codeExist) {
             throw new Error("Producto repetido");
         }
+
         const id = this.generarId();
-        this.products.push({ id, ...product });
+        this.products.push({ id, title, description, price, thumbnail, code, stock });
     }
 
     getProductById(id) {
-        const product = this.products.find(prod => prod.id === id);
+        const product = this.products.find(product => product.id === id);
         if (!product) {
             throw new Error("Not Found");
         }
@@ -28,7 +35,7 @@ class ProductManager {
         const id = this.uniqueId;
         this.uniqueId++
         return id;
-    };   
+    };
 
 }
 
@@ -44,20 +51,20 @@ const newProduct = {
     code: "1",
     stock: 30,
 };
-productManager.addProducts(newProduct);
+productManager.addProducts(newProduct.title, newProduct.description, newProduct.price, newProduct.thumbnail, newProduct.code, newProduct.stock);
 
 console.log(productManager.getProducts());
 
-try{
-    productManager.addProducts(newProduct);
-} catch (error){
+try {
+    productManager.addProducts(newProduct.title, newProduct.description, newProduct.price, newProduct.thumbnail, newProduct.code, newProduct.stock);
+} catch (error) {
     console.error(error.message);
 }
 
 const foundProduct = productManager.getProductById(1);
 
-try{
+try {
     productManager.getProductById(100);
-}catch(error){
+} catch (error) {
     console.error(error.message);
 }
