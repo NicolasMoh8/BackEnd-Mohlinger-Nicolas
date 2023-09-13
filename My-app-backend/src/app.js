375096d74dae8a8920c74b786448f90c47832acc
Nicolas Möhlinger <nicocmoh@gmail.com>
2023-09-12 21:00:30 -0300

    avance para entrega

--- My-app-backend/src/app.js
+++ My-app-backend/src/app.js
@@ -1,18 +1,26 @@
 import express from 'express';
+import __dirname from './utils.js';
+import handlebars from 'express-handlebars';
+import { Server } from 'socket.io';
+
 import productRouter from './routes/product.js';
 import cartsRouter from './routes/carts.js';
 
 const app = express();
-const PORT = 8080;
+const httpServer = app.listen(8080, () => console.log("Escuchando el puerto 8080"));
+const io = new Server(httpServer);
 
+
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views',__dirname+'/views');
+app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartsRouter);
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`)
-});
+
 
 
 
